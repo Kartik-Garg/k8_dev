c3bf5448238d866fa05055b0c60f32df2ee18d28
Kartik-Garg <kartikgarg938@gmail.com>
2022-10-07 13:53:16 +0530

    completed assignment 0

--- custom_controller/main.go
+++ custom_controller/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/kartik-garg/k8-dev/controller"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -12,21 +13,33 @@ import (
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfigfile", "/home/kartik/.kube/config", "This is where config file is located")
+	kubeconfig := flag.String("kubeconfig", "/home/kartik/.kube/config", "this is the location for your k8 config file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		fmt.Printf("We have error:%s, config file", err.Error())
+		fmt.Printf("Got an error:%s, while creating the kubeconfig file", err.Error())
 
+		//need to get the config file from inside the cluster, if this application is deployed on a cluster
 		config, err = rest.InClusterConfig()
 		if err != nil {
-			fmt.Printf("Error %s, in cluster config", err.Error())
+			fmt.Printf("Got an error:%s, while getting the config file from inside the cluster", err.Error())
 		}
 	}
+
+	//creating a clientset through config file
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		fmt.Printf("Error:%s, creating client set", err.Error())
+		fmt.Printf("Error %s, while creating clientset", err.Error())
 	}
 
+	ch := make(chan struct{})
+
 	informers := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
-	fmt.Print(informers)
+
+	c := controller.NewController(clientset, informers.Apps().V1().Deployments())
+	//start the informer as well
+	informers.Start(ch)
+	//run the controller now
+	c.Run(ch)
+	fmt.Println(informers)
+
 }
